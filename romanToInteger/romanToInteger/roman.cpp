2beba835c9db791501e6486a7bb3b4e018077b81
chai <304334894@qq.com>
2017-09-04 22:17:01 +0800

    update

---
+++ romanToInteger/romanToInteger/roman.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <Windows.h>
+
+
+#define FOR_MAX_NUM 4
+
+int romanToInt(char* s)
+{
+}
+
+int main()
+{
+	char c = 0;
+	char s[5] = { 0 };
+	int i = 0;
+	while ( (c = getchar()) != '\n')
+	{
+	}
+
+	int num = romanToInt(s);
+
+	printf("%d\n", num);
+	return 0;
+}
