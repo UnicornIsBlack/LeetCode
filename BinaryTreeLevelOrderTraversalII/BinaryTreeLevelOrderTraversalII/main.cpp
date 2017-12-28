ad20cd2ec4ea975934060fb4bc1b4cc3bcf3e17f
chai <304334894@qq.com>
2017-12-28 22:35:21 +0800

    update 17/12/28 22:30

--- BinaryTreeLevelOrderTraversalII/BinaryTreeLevelOrderTraversalII/main.cpp
+++ BinaryTreeLevelOrderTraversalII/BinaryTreeLevelOrderTraversalII/main.cpp
@@ -23,6 +23,19 @@ struct TreeNode
 	struct TreeNode *right;	
 };
 
+struct QueueNode
+{
+	struct QueueNode* next;
+	struct TreeNode* tNode;
+};
+
+struct Queue
+{
+	int n;
+	struct Queue* next;
+	struct QueueNode* fLastNode;
+};
+
 int maxDepth(struct TreeNode* root)
 {
 	if ( root == NULL )
@@ -37,6 +50,21 @@ int maxDepth(struct TreeNode* root)
 		return right;
 }
 
+void initQueue(struct Queue* queue, struct TreeNode* root)
+{
+	if ( root == NULL )
+		return;
+	queue->n++;
+	struct QueueNode* qNode = (struct QueueNode*)malloc(sizeof(struct QueueNode));
+	qNode->next = queue->fLastNode->next;
+	qNode->tNode = root;
+	queue->fLastNode->next = qNode;
+
+	initQueue(queue->next, root->right);
+	initQueue(queue->next, root->left);
+	return;
+}
+
 int** levelOrderBottom(struct TreeNode* root, int** columnSizes, int* returnSize)
 {
 	(*returnSize) = maxDepth(root);
@@ -45,10 +73,46 @@ int** levelOrderBottom(struct TreeNode* root, int** columnSizes, int* returnSize
 
 	if ( root == NULL )
 		return returnArray;
-
-	int size = 1;
-	int arrayEnd = (*returnSize) - 1;
 	
+	struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
+	queue->n = 0;
+	queue->fLastNode = (struct QueueNode*)malloc(sizeof(struct QueueNode));
+	queue->fLastNode->next = NULL;
+	queue->fLastNode->tNode = NULL;
+	struct Queue* q = queue;
+	q->next = NULL;
+
+	for ( int i = 1; i < (*returnSize); i++ )
+	{
+		struct Queue* p = (struct Queue*)malloc(sizeof(struct Queue));
+		p->n = 0;
+		p->fLastNode = (struct QueueNode*)malloc(sizeof(struct QueueNode));
+		p->fLastNode->next = NULL;
+		p->fLastNode->tNode = NULL;
+		p->next = NULL;
+		q->next = p;
+		q = p;
+	}
+
+	int num = (*returnSize) - 1;
+	q = queue;
+	while ( q != NULL )
+	{
+		returnArray[num] = (int*)malloc((q->n)*sizeof(int));
+		struct QueueNode* qNode = q->fLastNode->next;
+		struct QueueNode* nowNode = qNode;
+		for ( int j = 0; j < q->n; j++ )
+		{
+			returnArray[num][j] = qNode->tNode->val;
+			nowNode = qNode;
+			qNode = nowNode->next;
+			free(nowNode);
+		}
+		q = q->next;
+		free(queue);
+		queue = q;
+	}
+	return returnArray;
 
 }
 
