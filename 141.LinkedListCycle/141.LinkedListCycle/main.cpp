fc0280e90b28020ca74762704229af03eac2ea1e
chai <304334894@qq.com>
2018-01-12 19:45:42 +0800

    update 18/01/12 19:45

---
+++ 141.LinkedListCycle/141.LinkedListCycle/main.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+struct ListNode
+{
+	int val;
+	struct ListNode *next;
+};
+bool hasCycle(struct ListNode *head)/*¿ìÂýÖ¸Õë*/
+{
+	if ( head == NULL )
+		return false;
+	struct ListNode* slowNode = head;
+	struct ListNode* fastNode = head->next;
+	if ( slowNode == fastNode )
+		return true;
+	while ( fastNode != NULL )
+	{
+		slowNode = slowNode->next;
+		fastNode = fastNode->next;
+		if ( fastNode == NULL )
+			break;
+		if ( slowNode == fastNode )
+			return true;
+		fastNode = fastNode->next;
+		if ( slowNode == fastNode )
+			return true;
+	}
+	return false;
+
+}
+
+int main()
+{
+	return 0;
+}
