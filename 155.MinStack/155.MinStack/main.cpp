dfd623bf147914d044d0eb936cb0433cbd5e329d
chai <304334894@qq.com>
2018-01-13 22:29:29 +0800

    update 18/01/13 22:29

---
+++ 155.MinStack/155.MinStack/main.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct
+{
+} MinStack;
+
+/** initialize your data structure here. */
+MinStack* minStackCreate(int maxSize)
+{
+
+}
+
+void minStackPush(MinStack* obj, int x)
+{
+
+}
+
+void minStackPop(MinStack* obj)
+{
+
+}
+
+int minStackTop(MinStack* obj)
+{
+
+}
+
+int minStackGetMin(MinStack* obj)
+{
+
+}
+
+void minStackFree(MinStack* obj)
+{
+
+}
+
+/**
+* Your MinStack struct will be instantiated and called as such:
+* struct MinStack* obj = minStackCreate(maxSize);
+* minStackPush(obj, x);
+* minStackPop(obj);
+* int param_3 = minStackTop(obj);
+* int param_4 = minStackGetMin(obj);
+* minStackFree(obj);
+*/
+
+int main()
+{
+	return 0;
+}
