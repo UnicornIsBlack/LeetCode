a3d9f52b496d8701fc46864f8f3ba81e7f0c8861
chai <304334894@qq.com>
2018-01-24 16:52:08 +0800

    update 18/01/24 16:52

--- 203.RemoveLinkedListElements/203.RemoveLinkedListElements/main.cpp
+++ 203.RemoveLinkedListElements/203.RemoveLinkedListElements/main.cpp
@@ -9,9 +9,39 @@
 *     struct ListNode *next;
 * };
 */
+struct ListNode
+{
+	int val;
+	struct ListNode *next;
+};
 struct ListNode* removeElements(struct ListNode* head, int val)
 {
-
+	
+	struct ListNode* freeNode;
+	while ( head != NULL && head->val == val )
+	{
+		freeNode = head;
+		head = head->next;
+		free(freeNode);
+	}
+	if ( head == NULL )
+		return head;
+	struct ListNode* hClone = head;
+	while ( hClone->next != NULL )
+	{
+		if ( hClone->next->val == val )
+		{
+			freeNode = hClone->next;
+			hClone->next = freeNode->next;
+			free(freeNode);
+			continue;
+		}
+		else
+		{
+			hClone = hClone->next;
+		}
+	}
+	return head;
 }
 int main()
 {
