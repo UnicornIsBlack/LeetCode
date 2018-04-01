84587daea134d6b0185fe63dd12b00970578c6cb
chai <304334894@qq.com>
2018-04-01 18:57:35 +0800

    update 2018/04/01 18:57:34.89

--- 400.NthDigit/400.NthDigit/main.cpp
+++ 400.NthDigit/400.NthDigit/main.cpp
@@ -9,6 +9,7 @@ int returnInt(int n, int bit)
 	{
 		num *= 10;
 	}
+	printf("%d   \n", num);
 	int s = n / bit;
 	int m = n % bit;
 	if ( m > 0 )
@@ -17,6 +18,7 @@ int returnInt(int n, int bit)
 		m = bit;
 	num += (s - 1);
 	m = bit - m;
+	printf("m=%d,num=%d\n", m, num);
 	for ( int i = 0; i < bit; i++ )
 	{
 		val = num % 10;
@@ -31,23 +33,27 @@ int findNthDigit(int n)
 {
 	int num = 9;
 	int bit = 1; 
+	int upLim = num * bit;
 	int nReturn = 0;
 	while ( true )
 	{
-		if ( n <= num )
+		if ( n <= upLim || upLim <= 0 )
 		{
 			nReturn = returnInt(n, bit);
 			break;
 		}
-		n -= num;
+		printf("%d,%d,%d\n", n, bit,upLim);
+		n -= upLim;
 		num *= 10;
-		num += 9;
 		bit++;
+		upLim = num * bit;
 	}
 	return nReturn;
 }
 
 int main()
 {
+	int val = findNthDigit(1000000000);
+	printf("val=%d\n", val);
 	return 0;
 }
