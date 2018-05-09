245f451826ffa58c0955c6e51cb8e69de835d585
chai <304334894@qq.com>
2018-05-09 21:15:53 +0800

    update 2018/05/09 21:15:52.91

---
+++ 496.NextGreaterElementI/496.NextGreaterElementI/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+/**
+* Return an array of size *returnSize.
+* Note: The returned array must be malloced, assume caller calls free().
+*/
+int* nextGreaterElement(int* findNums, int findNumsSize, int* nums, int numsSize, int* returnSize)
+{
+
+}
+
+
+int main()
+{
+	return 0;
+}
