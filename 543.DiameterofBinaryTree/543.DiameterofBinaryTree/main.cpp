97767e07cc4bdbd96c6f10384b0cdef16d101d0e
chai <304334894@qq.com>
2018-06-21 17:49:12 +0800

    AC update 2018/06/21 17:49:12.20

---
+++ 543.DiameterofBinaryTree/543.DiameterofBinaryTree/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     struct TreeNode *left;
+*     struct TreeNode *right;
+* };
+*/
+struct TreeNode
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+
+int diameterOfBinaryTree(struct TreeNode* root)
+{
+	return 0;
+}
+
+int main()
+{
+	return 0;
+}
