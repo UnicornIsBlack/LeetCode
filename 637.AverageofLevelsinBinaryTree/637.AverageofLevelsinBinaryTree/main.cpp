e05edf976ce0b1bdaa822ad5b8b047c9b4f25033
chai <304334894@qq.com>
2018-09-05 17:14:22 +0800

    AC update 2018/09/05 17:14:22.04

--- 637.AverageofLevelsinBinaryTree/637.AverageofLevelsinBinaryTree/main.cpp
+++ 637.AverageofLevelsinBinaryTree/637.AverageofLevelsinBinaryTree/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -20,11 +21,34 @@ struct TreeNode
 	TreeNode *right;
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+
 class Solution
 {
 	public:
 	vector<double> averageOfLevels(TreeNode* root)
 	{
+		if ( !root )
+			return{};
+		vector<double> res;
+		queue<TreeNode*> q;
+		q.push(root);
 
+		while ( !q.empty() )
+		{
+			int n = q.size();
+			double sum = 0;
+			for ( int i = 0; i < n; i++ )
+			{
+				TreeNode* node = q.front();
+				q.pop();
+				sum += node->val;
+				if ( node->left )
+					q.push(node->left);
+				if ( node->right )
+					q.push(node->right);
+			}
+			res.push_back(sum / n);
+		}
+		return res;
 	}
 };
