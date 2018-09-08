f2577912f387e06b0b56d7e6834c80a2d23b9f0b
chai <304334894@qq.com>
2018-09-08 18:55:03 +0800

    update 2018/09/08 18:55:02.31

---
+++ 661.ImageSmoother/661.ImageSmoother/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+class Solution
+{
+	public:
+
+	vector<vector<int>> imageSmoother(vector<vector<int>>& M)
+	{
+		vector<vector<int>> smooth;
+		int rowNum = M.size();
+		int colNum = M[0].size();
+
+		for ( int i = 0; i < rowNum; i++ )
+		{
+			for ( int j = 0; j < colNum; j++ )
+			{
+
+			}
+		}
+	}
+};
