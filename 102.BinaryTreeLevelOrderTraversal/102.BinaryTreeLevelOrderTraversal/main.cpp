5c3ab0033e8ee1bfff88eef4fc1c9a8a1cf5189d
chai <304334894@qq.com>
2018-12-28 17:40:56 +0800

    AC update 2018/12/28 17:40:56.53

--- 102.BinaryTreeLevelOrderTraversal/102.BinaryTreeLevelOrderTraversal/main.cpp
+++ 102.BinaryTreeLevelOrderTraversal/102.BinaryTreeLevelOrderTraversal/main.cpp
@@ -28,8 +28,27 @@ class Solution
 	public:
 	vector<vector<int>> levelOrder(TreeNode* root)
 	{
-
+		vector<vector<int>> res;
+		if ( !root )
+			return res;
+		dfs(root, res, 1);
+		return res;
 	}
+	void dfs(TreeNode* root, vector<vector<int>>& res, int depth)
+	{
+		if ( !root )
+			return;
+		if ( res.size() < depth )
+		{
+			vector<int> leaf;
+			res.push_back(leaf);
+		}
+		dfs(root->left, res, depth + 1);
+		res[depth - 1].push_back(root->val);
+		dfs(root->right, res, depth + 1);
+	}
+
+
 };
 
 int main()
