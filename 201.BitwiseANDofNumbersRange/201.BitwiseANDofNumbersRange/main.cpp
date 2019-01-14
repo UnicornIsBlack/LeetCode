81c9d6165063e99ad13ab21cb31e68d8b1a59d30
chai <304334894@qq.com>
2019-01-14 14:31:05 +0800

    AC update 2019/01/14 14:31:05.52

---
+++ 201.BitwiseANDofNumbersRange/201.BitwiseANDofNumbersRange/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+	public:
+	int rangeBitwiseAnd(int m, int n)
+	{
+		int d = INT_MAX;
+		while ( (m & d) != (n & d) )
+		{
+			d <<= 1;
+		}
+		return m & d;
+	}
+};
+
+int main()
+{
+	return 0;
+}
