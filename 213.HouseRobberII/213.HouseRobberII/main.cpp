3424d182bcac871d367fc4db6ce9354b5642835e
chai <304334894@qq.com>
2019-02-19 23:01:23 +0800

    AC update 2019/02/19 23:01:21.24

--- 213.HouseRobberII/213.HouseRobberII/main.cpp
+++ 213.HouseRobberII/213.HouseRobberII/main.cpp
@@ -5,6 +5,33 @@
 
 using namespace std;
 
+class Solution
+{
+	public:
+	int rob(vector<int>& nums)
+	{
+		if ( nums.size() == 0 )
+			return 0;
+		if ( nums.size() == 1 )
+			return nums[0];
+		if ( nums.size() == 2 )
+			return max(nums[0], nums[1]);                  
+		vector<int> notrob(nums.size(), 0); //not rob the 0 index house
+		vector<int> rob(nums.size(), 0);	//rob the 0 index house
+		rob[0] = nums[0];
+		rob[1] = nums[0];
+		rob[2] = nums[0];
+		notrob[1] = nums[1];
+		notrob[2] = max(nums[1], nums[2]);
+		for ( int i = 3; i < nums.size(); i++ )
+		{
+			notrob[i] = max(notrob[i - 1], nums[i] + notrob[i - 2]);
+			rob[i] = max(nums[i - 1] + rob[i - 3], rob[i - 1]);
+		}
+		return max(notrob[nums.size() - 1], rob[nums.size() - 1]);
+	}
+};
+
 int main()
 {
 	return 0;
---
+++ 215.KthLargestElementinanArray/215.KthLargestElementinanArray/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+	public:
+	int findKthLargest(vector<int>& nums, int k)
+	{
+		for ( int i = 0; i < nums.size(); i++ )
+		{
+			for ( int j = 0; j < nums.size() - i; j++ )
+			{
+
+			}
+		}
+	}
+};
+
+int main()
+{
+	return 0;
+}
