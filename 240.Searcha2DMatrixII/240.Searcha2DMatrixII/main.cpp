4b464105a1b29e8f6cbbb73a8869d5dfa4addeeb
chai <304334894@qq.com>
2019-03-02 15:09:27 +0800

    AC update 2019/03/02 15:09:26.86

---
+++ 240.Searcha2DMatrixII/240.Searcha2DMatrixII/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+	public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target)
+	{
+		int row = matrix.size();
+		if ( row == 0 )
+			return false;
+		int col = matrix[0].size();
+		if ( col == 0 )
+			return false;
+		int i = 0;
+		int j = col - 1;
+		while ( i < row && j >= 0 )
+		{
+			if ( matrix[i][j] == target ) return true;
+			else if ( matrix[i][j] > target ) j--;
+			else i++;
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	return 0;
+}
