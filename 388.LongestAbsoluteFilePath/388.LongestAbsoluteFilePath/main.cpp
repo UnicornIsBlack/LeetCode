146e605fd555bb923c3fe5326e9ce4eeef8b0716
chai <304334894@qq.com>
2019-04-03 15:16:19 +0800

    AC update 2019/04/03 15:16:19.36

--- 388.LongestAbsoluteFilePath/388.LongestAbsoluteFilePath/main.cpp
+++ 388.LongestAbsoluteFilePath/388.LongestAbsoluteFilePath/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stack>
 #include <vector>
 #include <algorithm>
 
@@ -10,7 +11,44 @@ class Solution
 	public:
 	int lengthLongestPath(string input)
 	{
+		input += '\n';
+		int res = 0, spaceCount = 0, curPathLen = 0, len = input.size();
+		bool dotFlag = false;
+		stack<pair<string, int>> st;
+		string curDir, extension;
+		for ( int i = 0; i < len; i++ )
+		{
+			if ( input[i] != '\n' )
+			{
+				if ( input[i] != '\t' )
+					curDir += input[i];
+				if ( dotFlag )
+					extension += input[i];
+				if ( input[i] == '\t' )
+					spaceCount++;
+				if ( input[i] == '.' )
+					dotFlag = true;
+				continue;
+			}
 
+			while ( !st.empty() && spaceCount <= st.top().second )
+			{
+				curPathLen -= st.top().first.size();
+				st.pop();
+			}
+			if ( dotFlag )
+				res = max(curPathLen + (int)curDir.size() + 1, res);
+			else
+			{
+				st.push(make_pair("/" + curDir, spaceCount));
+				curPathLen += curDir.size() + 1;
+			}
+			extension = "";
+			dotFlag = false;
+			spaceCount = 0;
+			curDir = "";
+		}
+		return res == 0 ? 0 : res - 1;
 	}
 };
 
