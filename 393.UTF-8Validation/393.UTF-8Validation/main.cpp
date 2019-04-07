efcbef68cc75e2eed5eb08cc42d26c516a1bfe7b
chai <304334894@qq.com>
2019-04-07 20:26:43 +0800

    AC update 2019/04/07 20:26:42.47

--- 393.UTF-8Validation/393.UTF-8Validation/main.cpp
+++ 393.UTF-8Validation/393.UTF-8Validation/main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
+
 
 using namespace std;
+
 
 class Solution
 {
 	public:
 	bool validUtf8(vector<int>& data)
 	{
-
+		int count = 0;
+		for ( auto c : data )
+		{
+			if ( count == 0 )
+			{
+				if ( (c >> 5) == 0x06 )count = 1;
+				else if ( (c >> 4) == 0x0E )count = 2;
+				else if ( (c >> 3) == 0x1E )count = 3;
+				else if ( (c >> 7) )return false;
+			}
+			else
+			{
+				if ( (c >> 6) != 0x02 )return false;
+				count--;
+			}
+		}
+		return count == 0;
 	}
 };
 
