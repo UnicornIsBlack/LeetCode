8758a53bef54ce97e924e669d2d19b9d927a8b38
chai <304334894@qq.com>
2019-04-15 18:25:54 +0800

    AC update 2019/04/15 18:25:53.92

--- 416.PartitionEqualSubsetSum/416.PartitionEqualSubsetSum/main.cpp
+++ 416.PartitionEqualSubsetSum/416.PartitionEqualSubsetSum/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <numeric>
 #include <algorithm>
 
 using namespace std;
@@ -10,6 +11,20 @@ class Solution
 	public:
 	bool canPartition(vector<int>& nums)
 	{
+
+		int target = accumulate(nums.begin(), nums.end(), 0);
+		if ( target % 2 == 1 )
+			return false;
+		target >>= 1;
+		vector<bool> flag(target + 1, false);
+		flag[0] = true;
+		
+		for ( int num : nums )
+		{
+			for ( int i = target; i >= num; i-- )
+				flag[i] = flag[i] | flag[i - num];
+		}
+		return flag[target];
 
 	}
 };
---
+++ 417.PacificAtlanticWaterFlow/417.PacificAtlanticWaterFlow/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+	public:
+	vector<pair<int, int>> pacificAtlantic(vector<vector<int>>& matrix)
+	{
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
